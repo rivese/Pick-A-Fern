7cb566112c9cba4ed75eef4a4d4155aaa2abb830
rivese <erives@umich.edu>
2014-08-19 15:23:52 -0700

    add jquery to javascript file

--- pick_a_fern.js
+++ pick_a_fern.js
@@ -1,9 +1,22 @@
-function changeHeading() {
-  var elHeading = document.getElementById('title');
-  elHeading.textContent = 'Here\'s Your Fern!';
-  elHeading.style.color = 'white';
-  elHeading.style.fontSize = '6em';
-}
+$(function() {
+
+  $('html').on('click', '.fern-image', function() {
+    $('.page-title').html('<h1>Here\'s Your Fern!</h1>');
+    $('.page-title').css({'color': 'white', 'font-size': '3em'});
+
+    $('body').css({'background': 'url(images/star_2.gif) repeat', 'background-color': '#d82b86'});
+
+    $('#fern_music').get(0).play();
+
+  });
+
+  $('html').on('click', '#tree', function() {
+     $('.page-title').html('That\'s Not a Fern!');
+     $('.page-title').css({'color': 'white'});
+  });
+
+});
+
 
 function fernDisplay() {
   var elDivs = document.getElementsByClassName('fern-gallery__fern');
@@ -23,32 +36,20 @@ function fernDisplay() {
   elTree.style.display = 'none';
 }
 
-function displayStars() {
-  document.body.style.background = 'url(images/star_2.gif) repeat';
-  document.body.style.backgroundColor = '#d82b86';
-}
-
-function treeClick() {
-  var elHeading = document.getElementById('title');
-  elHeading.textContent = 'That\'s Not a Fern!';
-  elHeading.style.color = 'white';
-}
-
-function playMusic() {
-  var elTrack = document.getElementById('fern_music');
-  elTrack.play();
-}
-
-var elTree = document.getElementById('tree');
-elTree.addEventListener('click', treeClick, false);
 
 var elImages = document.getElementsByClassName('fern-image');
 for (i = 0; i < elImages.length; i++) {
-  elImages[i].addEventListener('click', changeHeading, false);
   elImages[i].addEventListener('click', fernDisplay, false);
-  elImages[i].addEventListener('click', displayStars, false);
-  elImages[i].addEventListener('click', playMusic, false);
 }
+
+
+
+
+
+    
+
+  
+
 
 
 
